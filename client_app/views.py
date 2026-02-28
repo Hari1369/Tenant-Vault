036c3e18fb77d85ec61d863fb85e2dec4e560ec0
Hari1369 <hiranmaymondal369@gmail.com>
2026-02-28 20:22:55 +0530

    added with Final Project

--- client_app/views.py
+++ client_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from client_app.models import Employee
 # Create your views here.
+
+
+def index(request):
+    employee = Employee.objects.all()
+    return render(request, "client_index.html", {"employees":employee})
+
+def create_employee(request):
+    if request.POST:
+        name = request.POST.get("name")
+        employee = Employee(name=name)
+        employee.save()
+    return redirect("client_index")
