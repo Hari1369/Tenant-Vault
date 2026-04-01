e14bd9475d14d858c8281a4c903aad0b5eda87db
Hari1369 <hiranmaymondal369@gmail.com>
2026-04-01 20:52:33 +0530

    added with domain configurations

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import index
+from app.views import index, check_tenant
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    
+    
+    # # ====================================================> TESTING
+    # path('', check_tenant),
+    # # ====================================================> TESTING
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Public index</h1>")
+
+# # ====================================================> TESTING
+# def check_tenant(request):
+#     print(f"Tenant : {request.tenant.schema_name}")
+#     return HttpResponse(
+#         f"Tenant: {request.tenant.schema_name}, Host: {request.get_host()}"
+#     )
+# # ====================================================> TESTING
