cf56c4ac660e31dd746e2bd4b3b6ef434b517267
Hari1369 <hiranmaymondal369@gmail.com>
2026-04-02 13:56:23 +0000

    adding with log-in page

--- app/views.py
+++ app/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .models import SuperAdmin
 
 def index(request):
     return HttpResponse("<h1>Public index</h1>")
+
+def log_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            profile = user.SuperAdmin
+            if profile.role == 'super_admin':
+                return redirect('admin_dashboard')
+            else:
+                return redirect('dashboard')
+    else:
+
 
 # # ====================================================> TESTING
 # def check_tenant(request):
@@ -11,3 +29,5 @@ def index(request):
 #         f"Tenant: {request.tenant.schema_name}, Host: {request.get_host()}"
 #     )
 # # ====================================================> TESTING
+
+
