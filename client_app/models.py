8a3c93fb9d2bfe4354efa69df21bf9051f14d589
Hari1369 <hiranmaymondal369@gmail.com>
2026-04-03 22:47:02 +0530

    added with new migrations with employe model

--- client_app/models.py
+++ client_app/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from app.models import Domain, Client
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your models here.
 class Employee(models.Model):
     name = models.CharField(max_length=250)
-    
+    password = models.CharField(max_length=128)
+    client = models.ForeignKey(
+        'app.Client',
+        on_delete=models.CASCADE,
+        related_name='employees'
+    )
+
     class Meta:
         db_table = "employee"
-        
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
 
 class TenantUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
--- client_app/views.py
+++ client_app/views.py
@@ -1,16 +1,46 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from client_app.models import Employee
+from .forms import EmployeeForm
+from django.contrib import messages
+
 # Create your views here.
-
-
 def index(request):
     employee = Employee.objects.all()
-    return render(request, "client_index.html", {"employees":employee})
+    form = EmployeeForm()
+
+    return render(request, "client_index.html", {
+        "employees": employee,
+        "form": form
+    })
+    
+    
 
 def create_employee(request):
-    if request.POST:
-        name = request.POST.get("name")
-        employee = Employee(name=name)
-        employee.save()
-    return redirect("client_index")
+    if request.method == "POST":
+        form = EmployeeForm(request.POST)
+        
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            password = form.cleaned_data["password"]
+            
+            employee = Employee(
+                name=name,
+                client=request.tenant
+            )
+            employee.set_password(password)
+            employee.save()
+            
+            # ✅ success message
+            messages.success(request, "Employee added to system successfully!")
+
+            return redirect("client_index")
+
+    else:
+        form = EmployeeForm()
+
+    employees = Employee.objects.all()
+    return render(request, "client_index.html", {
+        "form": form,
+        "employees": employees
+    })
