91aeea25edb831db92547520ad48e3b81b22b0a4
Hari1369 <hiranmaymondal369@gmail.com>
2026-04-07 13:26:53 +0530

    added with log setup and forms settings

--- client_app/urls.py
+++ client_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
-from .views import index, create_employee
+from .views import index, create_employee, tenant_log_in, index_page
 
 urlpatterns = [
-    path('', index, name="client_index"),
+    path('', tenant_log_in, name="tenant_login"),
+    path('client_dashboard', index_page, name="client_dashboard"),
+    path('employee_add', index, name="client_index"),
     path('create_employee', create_employee, name="create_employee")
 ]
